0ccb4e2ead6c916a939fa58ea58973d4ccfe249c
Stefano <stc@MBpro-di-sc.local>
2025-12-10 01:16:26 +0100

    fix provider.js darkmode

--- components/Providers.js
+++ components/Providers.js
@@ -1,38 +1,46 @@
 'use client'
 
+import { ThemeProvider } from 'next-themes'
 import { Toaster } from 'react-hot-toast'
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 
 export function Providers({ children }) {
   return (
-    <TooltipPrimitive.Provider delayDuration={300}>
-      {children}
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          // Stile default
-          duration: 4000,
-          style: {
-            background: '#333',
-            color: '#fff',
-            borderRadius: '10px',
-            padding: '16px',
-          },
-          // Stili per tipo
-          success: {
-            iconTheme: {
-              primary: '#10b981',
-              secondary: '#fff',
+    <ThemeProvider 
+      attribute="class" 
+      defaultTheme="system" 
+      enableSystem
+      disableTransitionOnChange
+    >
+      <TooltipPrimitive.Provider delayDuration={300}>
+        {children}
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            // Stile default
+            duration: 4000,
+            style: {
+              background: '#333',
+              color: '#fff',
+              borderRadius: '10px',
+              padding: '16px',
             },
-          },
-          error: {
-            iconTheme: {
-              primary: '#ef4444',
-              secondary: '#fff',
+            // Stili per tipo
+            success: {
+              iconTheme: {
+                primary: '#10b981',
+                secondary: '#fff',
+              },
             },
-          },
-        }}
-      />
-    </TooltipPrimitive.Provider>
+            error: {
+              iconTheme: {
+                primary: '#ef4444',
+                secondary: '#fff',
+              },
+            },
+          }}
+        />
+      </TooltipPrimitive.Provider>
+    </ThemeProvider>
   )
 }
